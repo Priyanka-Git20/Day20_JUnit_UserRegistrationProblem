09b934ed5888a1517575dbd21b150369168eeb3b
priyanka <priyanka.chavan1408@gmail.com>
2022-02-25 23:06:36 +0530

    ADDED:Checking the name entered by the user is valid or not using test cases.

--- main/java/com/bridgelabz/junit/UserRegistration.java
+++ main/java/com/bridgelabz/junit/UserRegistration.java
@@ -1,7 +1,12 @@
 package com.bridgelabz.junit;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the user registration testing");
+    public boolean firstName(String firstName){
+        Pattern pattern = Pattern.compile("^[A-Z][a-z]{2,}+$");
+        Matcher matcher = pattern.matcher(firstName);
+        return matcher.matches();
     }
 }
